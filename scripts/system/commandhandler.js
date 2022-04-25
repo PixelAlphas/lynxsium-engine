87918c7075d9f42cfb4f03389f2efd81f3f4d62b
AetherHarlow <88525994+PixelAlphas@users.noreply.github.com>
2022-04-25 18:01:16 +0700

    Create commandhandler.js

---
+++ scripts/system/commandhandler.js
@@ -0,0 +1,35 @@
+// imports config for prefix.
+import { prefix } from "../config.js"
+
+// export and consts custom commands as functions.
+// using switch as for optimizations.
+
+// The original version of lynxsium scripts is actually
+// more customizable and flexible. but unfortunately how
+// complex me and my friend made, we have to create
+// that as switch code. this way for optimization but less
+// customizable/flexible.
+
+// >] code example [<
+// case 'example' - commands name.
+//   { code here. }
+//   break;
+
+export const customcommand = function (message, player, eventData) {
+  let command = message.trim().slice(prefix.length).split(/ +/)[0].toLowerCase(); 
+  switch (command) {
+    case 'hello':{
+      console.warn("hello commands ran");
+      break;
+    }
+    case "helloadmin":{
+      if (!player.hasTag("admin")) return console.warn("hello admin ran but no permissions.");
+      console.warn("hello admin ran :D");
+      break;
+    }
+    default:{
+      console.warn("unknown commands");
+      break;
+    }
+  }
+};
