f257251ba4c1a2707482c34ed79a11e7575cbf74
AetherHarlow <88525994+PixelAlphas@users.noreply.github.com>
2022-04-25 14:34:02 +0700

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
+// Imports Minecraft's library.
+import { world } from "mojang-minecraft";
 
+// prefix's for admin/member commands.
+var prefixmember = "!"
+var prefixadmin = "-"
+
+world.events.beforeChat.subscribe(eventData => {
+  // Won't send any chat messages but continues to functionn
+  eventData.cancel = true;
+  
+  // consts both player and messages
+  const player = eventData.sender;
+  const message = eventData.message;
+
+  // detects if both prefix did not work.
+  if (!message.startsWith(prefixmember) && !message.startsWith(prefixadmin))
+  return console.warn("commands ran.")
+  
+  console.warn('chat inputed.')
+});
