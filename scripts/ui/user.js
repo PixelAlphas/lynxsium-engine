4e8e283245baff903fd4fc908a953bd1abd5be33
AetherHarlow <88525994+PixelAlphas@users.noreply.github.com>
2022-04-26 13:29:05 +0700

    update user.js

--- scripts/ui/user.js
+++ scripts/ui/user.js
@@ -1,1 +1,7 @@
-import
+import * as ui from "mojang-minecraft-ui";
+
+export function LynxMenu(player) {
+  let lynxui = ui.ActionFormData();
+  lynxui.title("Title Test");
+  lynxui.body("Body Test");
+}
